0524f9c87d55702e01837fc89b3923abfe200fdd
Dafiedoe <djvanrijn@gmail.com>
2025-07-18 13:52:20 +0200

    Deleted NPC code. Going to use EasyNPC.

--- src/main/java/net/valdora/ValdoraClient.java
+++ src/main/java/net/valdora/ValdoraClient.java
@@ -1,13 +1,9 @@
 package net.valdora;
 
-import net.valdora.npc.ModNPC;
-import net.valdora.npc.custom.StaticNPCRenderer;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 
 public class ValdoraClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        EntityRendererRegistry.register(ModNPC.STATIC_NPC, StaticNPCRenderer::new);
     }
 }
