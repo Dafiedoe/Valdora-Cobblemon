68be9db587d07b9513a7f5a5f01974d80671a7ec
Dafiedoe <djvanrijn@gmail.com>
2025-09-22 17:27:36 +0200

    Fixed bug

--- src/main/java/net/valdora/trainers/events/TrainerBattleEndEvent.java
+++ src/main/java/net/valdora/trainers/events/TrainerBattleEndEvent.java
@@ -2,11 +2,9 @@ package net.valdora.trainers.events;
 
 import com.cobblemon.mod.common.api.Priority;
 import com.cobblemon.mod.common.api.battles.model.PokemonBattle;
+import com.cobblemon.mod.common.api.battles.model.actor.BattleActor;
 import com.cobblemon.mod.common.api.events.CobblemonEvents;
-import com.cobblemon.mod.common.api.events.battles.BattleFaintedEvent;
-import com.cobblemon.mod.common.api.events.battles.BattleFledEvent;
 import com.cobblemon.mod.common.api.events.battles.BattleVictoryEvent;
-import com.cobblemon.mod.common.battles.BattleSide;
 import com.cobblemon.mod.common.battles.actor.PlayerBattleActor;
 import com.mojang.brigadier.ParseResults;
 import kotlin.Unit;
@@ -19,157 +17,52 @@ import java.util.UUID;
 
 public class TrainerBattleEndEvent {
     public static void register() {
-        CobblemonEvents.BATTLE_FLED.subscribe(Priority.NORMAL, TrainerBattleEndEvent::battleFled);
-        //CobblemonEvents.BATTLE_FAINTED.subscribe(Priority.NORMAL, TrainerBattleEndEvent::battleFaint);
         CobblemonEvents.BATTLE_VICTORY.subscribe(Priority.NORMAL, TrainerBattleEndEvent::battleVictory);
-    }
-
-    private static Unit battleFled(BattleFledEvent battleFledEvent) {
-        PokemonBattle battle = battleFledEvent.getBattle();
-        battleLost(battle);
-        return null;
-    }
-
-    private static Unit battleFaint(BattleFaintedEvent battleFaintedEvent) {
-        PokemonBattle battle = battleFaintedEvent.getBattle();
-
-        PlayerBattleActor playerActor = null;
-        BattleSide firstSide = battle.getSide1();
-        BattleSide secondSide = battle.getSide2();
-
-        if (firstSide.getActors()[0] instanceof PlayerBattleActor playerBattleActor) {
-            playerActor = playerBattleActor;
-        } else if (secondSide.getActors()[0] instanceof PlayerBattleActor playerBattleActor) {
-            playerActor = playerBattleActor;
-        }
-
-        if (playerActor != null && isPlayerSideDefeated(playerActor)) {
-            battleLost(battle);
-        }
-
-        return null;
-    }
-
-    private static boolean isPlayerSideDefeated(PlayerBattleActor playerActor) {
-        return playerActor.getPokemonList().stream().allMatch(pokemon -> pokemon.getHealth() <= 0);
-    }
-
-    private static void battleLost(PokemonBattle battle) {
-        BattleSide firstSide = battle.getSide1();
-        BattleSide secondSide = battle.getSide2();
-
-        if (firstSide.getActors()[0] instanceof PlayerBattleActor playerBattleActor) {
-            if (playerBattleActor.getEntity() != null) {
-                CheckPointManager.recallPlayerToCheckPoint(playerBattleActor.getEntity());
-            }
-        }
-        if (secondSide.getActors()[0] instanceof PlayerBattleActor playerBattleActor) {
-            if (playerBattleActor.getEntity() != null) {
-                CheckPointManager.recallPlayerToCheckPoint(playerBattleActor.getEntity());
-            }
-        }
-
-        if (firstSide.getActors()[0] instanceof PokemonTeamBattleActor pokemonBattleActor &&
-                secondSide.getActors()[0] instanceof PlayerBattleActor playerBattleActor) {
-            UUID playerUuid = playerBattleActor.getEntity().getUuid();
-            PlayerSaveDataManager.PlayerStoryProgress progress = PlayerSaveDataManager.INSTANCE.getProgress(playerBattleActor.getEntity().getServer(), playerUuid);
-            String flag = pokemonBattleActor.getId().toLowerCase();
-            progress.setFlag(flag, "defeat");
-            PlayerSaveDataManager.INSTANCE.saveProgress(playerBattleActor.getEntity().getServer(), playerUuid);
-
-            String command = "easy_npc dialog open " + pokemonBattleActor.getNpcUuid() + " " + playerBattleActor.getEntity().getName().getString() + " " + flag + "_ondefeat";
-            ServerCommandSource source = playerBattleActor.getEntity().getServer().getCommandSource().withLevel(2);
-            try {
-                ParseResults<ServerCommandSource> parseResults = playerBattleActor.getEntity().getServer().getCommandManager().getDispatcher().parse(command, source);
-                playerBattleActor.getEntity().getServer().getCommandManager().execute(parseResults, command);
-            } catch (Exception e) {
-                Valdora.LOGGER.error("Failed to execute command: {}", command, e);
-            }
-        }
-        if (secondSide.getActors()[0] instanceof PokemonTeamBattleActor pokemonBattleActor &&
-                firstSide.getActors()[0] instanceof PlayerBattleActor playerBattleActor) {
-            UUID playerUuid = playerBattleActor.getEntity().getUuid();
-            PlayerSaveDataManager.PlayerStoryProgress progress = PlayerSaveDataManager.INSTANCE.getProgress(playerBattleActor.getEntity().getServer(), playerUuid);
-            String flag = pokemonBattleActor.getId().toLowerCase();
-            progress.setFlag(flag, "defeat");
-            PlayerSaveDataManager.INSTANCE.saveProgress(playerBattleActor.getEntity().getServer(), playerUuid);
-
-            String command = "easy_npc dialog open " + pokemonBattleActor.getNpcUuid() + " " + playerBattleActor.getEntity().getName().getString() + " " + flag + "_ondefeat";
-            ServerCommandSource source = playerBattleActor.getEntity().getServer().getCommandSource().withLevel(2);
-            try {
-                ParseResults<ServerCommandSource> parseResults = playerBattleActor.getEntity().getServer().getCommandManager().getDispatcher().parse(command, source);
-                playerBattleActor.getEntity().getServer().getCommandManager().execute(parseResults, command);
-            } catch (Exception e) {
-                Valdora.LOGGER.error("Failed to execute command: {}", command, e);
-            }
-        }
     }
 
     private static Unit battleVictory(BattleVictoryEvent battleVictoryEvent) {
         PokemonBattle battle = battleVictoryEvent.getBattle();
 
-        boolean playerWon = false;
-        BattleSide firstSide = battle.getSide1();
-        BattleSide secondSide = battle.getSide2();
+        battleVictoryEvent.getLosers().stream()
+                .filter(actor -> actor instanceof PlayerBattleActor)
+                .map(actor -> ((PlayerBattleActor) actor).getEntity())
+                .forEach(CheckPointManager::recallPlayerToCheckPoint);
+
+        BattleActor actor1 = battle.getSide1().getActors()[0];
+        BattleActor actor2 = battle.getSide2().getActors()[0];
+
         PlayerBattleActor playerActor = null;
+        PokemonTeamBattleActor trainerActor = null;
 
-        if (firstSide.getActors()[0] instanceof PlayerBattleActor playerBattleActor) {
-            playerActor = playerBattleActor;
-        } else if (secondSide.getActors()[0] instanceof PlayerBattleActor playerBattleActor) {
-            playerActor = playerBattleActor;
+        if (actor1 instanceof PlayerBattleActor && actor2 instanceof PokemonTeamBattleActor) {
+            playerActor = (PlayerBattleActor) actor1;
+            trainerActor = (PokemonTeamBattleActor) actor2;
+        } else if (actor2 instanceof PlayerBattleActor && actor1 instanceof PokemonTeamBattleActor) {
+            playerActor = (PlayerBattleActor) actor2;
+            trainerActor = (PokemonTeamBattleActor) actor1;
         }
 
-        if (playerActor != null && battleVictoryEvent.getWinners().contains(playerActor)) {
-            playerWon = true;
-        }
+        if (playerActor != null && trainerActor != null) {
+            boolean playerWon = battleVictoryEvent.getWinners().contains(playerActor);
+            String status = playerWon ? "victory" : "defeat";
+            String dialogSuffix = playerWon ? "_onvictory" : "_ondefeat";
 
-        if (playerWon) {
-            battleWon(battle);
-        } else {
-            battleLost(battle);
+            UUID playerUuid = playerActor.getEntity().getUuid();
+            PlayerSaveDataManager.PlayerStoryProgress progress = PlayerSaveDataManager.INSTANCE.getProgress(playerActor.getEntity().getServer(), playerUuid);
+            String flag = trainerActor.getId().toLowerCase();
+            progress.setFlag(flag, status);
+            PlayerSaveDataManager.INSTANCE.saveProgress(playerActor.getEntity().getServer(), playerUuid);
+
+            String command = "easy_npc dialog open " + trainerActor.getNpcUuid() + " " + playerActor.getEntity().getName().getString() + " " + flag + dialogSuffix;
+            ServerCommandSource source = playerActor.getEntity().getServer().getCommandSource().withLevel(2);
+            try {
+                ParseResults<ServerCommandSource> parseResults = playerActor.getEntity().getServer().getCommandManager().getDispatcher().parse(command, source);
+                playerActor.getEntity().getServer().getCommandManager().execute(parseResults, command);
+            } catch (Exception e) {
+                Valdora.LOGGER.error("Failed to execute command: {}", command, e);
+            }
         }
 
         return null;
-    }
-
-    private static void battleWon(PokemonBattle battle) {
-        BattleSide firstSide = battle.getSide1();
-        BattleSide secondSide = battle.getSide2();
-
-        if (firstSide.getActors()[0] instanceof PokemonTeamBattleActor pokemonBattleActor &&
-                secondSide.getActors()[0] instanceof PlayerBattleActor playerBattleActor) {
-            UUID playerUuid = playerBattleActor.getEntity().getUuid();
-            PlayerSaveDataManager.PlayerStoryProgress progress = PlayerSaveDataManager.INSTANCE.getProgress(playerBattleActor.getEntity().getServer(), playerUuid);
-            String flag = pokemonBattleActor.getId().toLowerCase();
-            progress.setFlag(flag, "victory");
-            PlayerSaveDataManager.INSTANCE.saveProgress(playerBattleActor.getEntity().getServer(), playerUuid);
-
-            String command = "easy_npc dialog open " + pokemonBattleActor.getNpcUuid() + " " + playerBattleActor.getEntity().getName().getString() + " " + flag + "_onvictory";
-            ServerCommandSource source = playerBattleActor.getEntity().getServer().getCommandSource().withLevel(2);
-            try {
-                ParseResults<ServerCommandSource> parseResults = playerBattleActor.getEntity().getServer().getCommandManager().getDispatcher().parse(command, source);
-                playerBattleActor.getEntity().getServer().getCommandManager().execute(parseResults, command);
-            } catch (Exception e) {
-                Valdora.LOGGER.error("Failed to execute command: {}", command, e);
-            }
-        }
-        if (secondSide.getActors()[0] instanceof PokemonTeamBattleActor pokemonBattleActor &&
-                firstSide.getActors()[0] instanceof PlayerBattleActor playerBattleActor) {
-            UUID playerUuid = playerBattleActor.getEntity().getUuid();
-            PlayerSaveDataManager.PlayerStoryProgress progress = PlayerSaveDataManager.INSTANCE.getProgress(playerBattleActor.getEntity().getServer(), playerUuid);
-            String flag = pokemonBattleActor.getId().toLowerCase();
-            progress.setFlag(flag, "victory");
-            PlayerSaveDataManager.INSTANCE.saveProgress(playerBattleActor.getEntity().getServer(), playerUuid);
-
-            String command = "easy_npc dialog open " + pokemonBattleActor.getNpcUuid() + " " + playerBattleActor.getEntity().getName().getString() + " " + flag + "_onvictory";
-            ServerCommandSource source = playerBattleActor.getEntity().getServer().getCommandSource().withLevel(2);
-            try {
-                ParseResults<ServerCommandSource> parseResults = playerBattleActor.getEntity().getServer().getCommandManager().getDispatcher().parse(command, source);
-                playerBattleActor.getEntity().getServer().getCommandManager().execute(parseResults, command);
-                Valdora.LOGGER.info("Server executed command: {}", command);
-            } catch (Exception e) {
-                Valdora.LOGGER.error("Failed to execute command: {}", command, e);
-            }
-        }
     }
 }
